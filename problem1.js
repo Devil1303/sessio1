5bb1f80378c1b0b15294391bee6dd11b13c0189c
Rahul giri <Sumit.giri2001@gmail.com>
2023-04-10 13:52:23 +0530

    even-odd

---
+++ problem1.js
@@ -0,0 +1,15 @@
+//if i want to check a number is even or odd;
+
+
+// if the reminder is 0 we can say is even.
+
+let number=4;
+
+if (number%2==0){
+
+console.log("even number");
+}
+
+even{
+console.log("odd number");
+}
